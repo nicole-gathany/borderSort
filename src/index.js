623cc2dcfcfa21a2c2fc145a39d49379713a24a0
Nicole Gathany <nicole.gathany@gmail.com>
2020-02-21 08:01:35 -0600

    loops and empty array
    added loops and empty array

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-let borderSort = function(a) {};
+let borderSort = function(arr) {
+  //two loops because it's a nested array
+  let arr2 = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {}
+  }
+};
 
 borderSort([
   [15, -66, -18, 99],
@@ -6,3 +12,5 @@ borderSort([
   [-59, -37, 82, -33],
   [-15, 26, 61, -2]
 ]);
+
+borderSort([[9, 4], [6, 8]]);
